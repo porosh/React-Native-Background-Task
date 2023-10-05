e0d0a8d9b09b62d2daf9272847396297e00d78d8
Nur E Alam Khan <porosh@technobd.com>
2023-10-05 15:24:05 +0600

    update App.js

--- App.js
+++ App.js
@@ -17,59 +17,9 @@ import {
   Platform,
   Linking,
 } from 'react-native';
-
-// //import BackgroundJob from 'react-native-background-actions';
 import BackgroundService from 'react-native-background-actions';
 
-// //const sleep = (time) => new Promise<void>((resolve) => setTimeout(() => resolve(), time));
-// const sleep = time => new Promise(resolve => setTimeout(() => resolve(), time));
-
-// BackgroundJob.on('expiration', () => {
-//   console.log('iOS: I am being closed!');
-// });
-
-// const taskRandom = async taskData => {
-//   console.log('taskRandom called ');
-//   if (Platform.OS === 'ios') {
-//     console.warn(
-//       'This task will not keep your app alive in the background by itself, use other library like react-native-track-player that use audio,',
-//       'geolocalization, etc. to keep your app alive in the background while you excute the JS from this library.',
-//     );
-//   }
-//   await new Promise(async resolve => {
-//     // For loop with a delay
-//     const {delay} = taskData;
-//     console.log(BackgroundJob.isRunning(), delay);
-//     for (let i = 0; BackgroundJob.isRunning(); i++) {
-//       console.log('Runned -> ', i);
-//       await BackgroundJob.updateNotification({taskDesc: 'Runned -> ' + i});
-//       await sleep(delay);
-//     }
-//     resolve();
-//   });
-// };
-
-// const options = {
-//   taskName: 'Example',
-//   taskTitle: 'ExampleTask title',
-//   taskDesc: 'ExampleTask desc',
-//   taskIcon: {
-//     name: 'ic_launcher',
-//     type: 'mipmap',
-//   },
-//   color: '#ff00ff',
-//   linkingURI: 'exampleScheme://chat/jane',
-//   parameters: {
-//     delay: 1000,
-//   },
-// };
-
 export default App = () => {
-  const usingHermes =
-    typeof HermesInternal === 'object' && HermesInternal !== null;
-
-  //let playing = BackgroundJob.isRunning();
-
   const sleep = time =>
     new Promise(resolve => setTimeout(() => resolve(), time));
 
@@ -116,65 +66,14 @@ export default App = () => {
     mBackgroundTask();
   }, []);
 
-  /**
-   * Toggles the background task
-   */
-  // const toggleBackground = async () => {
-  //   playing = !playing;
-  //   if (playing) {
-  //     try {
-  //       console.log('Trying to start background service');
-  //       await BackgroundJob.start(taskRandom, options);
-  //       console.log('Successful start!');
-  //     } catch (e) {
-  //       console.log('Error', e);
-  //     }
-  //   } else {
-  //     console.log('Stop background service');
-  //     await BackgroundJob.stop();
-  //   }
-  // };
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          {/* <Header /> */}
-          {!usingHermes ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <TouchableOpacity
-              style={{height: 100, width: 100, backgroundColor: 'red'}}
-              onPress={() => {}}></TouchableOpacity>
-          </View>
-        </ScrollView>
+        <View>
+          <Text>Background Service</Text>
+        </View>
       </SafeAreaView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: '#fff',
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: '#fff',
-  },
-  footer: {
-    color: '#fff',
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
